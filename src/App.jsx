3fda949c3d46e709c3f1ed0c14d451ab92a9ea3d
alakhagarwal <agarwalalakh2004@gmail.com>
2025-09-07 18:40:17 +0530

    Added the Click Handler.

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./App.css";
+import Gamespace from "./components/Gamespace.jsx";
+// Ensure Tailwind CSS is imported in your main CSS file (e.g., index.css or App.css)
+
+function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [level, setLevel] = useState(8);
+
+  return (
+    <div className="my-container text-white max-w-[1600px] mx-auto w-full p-4">
+      <header className="text-7xl px-10 flex justify-between">
+        <h1 className="text-red-700 font-bold ">
+          Poke<span className="text-white">Game</span>
+        </h1>
+
+        <select
+          className="bg-gray-800 text-white text-xl rounded px-4 py-2 mt-6 cursor-pointer"
+          defaultValue={8}
+          onChange={(e) => {
+            setLevel(e.target.value);
+            console.log("The level is " + e.target.value);
+          }}
+        >
+          <option value={8}>Easy</option>
+          <option value={12}>Medium</option>
+          <option value={20}>Hard</option>
+        </select>
+      </header>
+
+      <div className="details text-[1.2rem] details p-10 m-auto text-center">
+        <h5>
+          Get points by clicking on an image but don't click on any more than
+          once!
+        </h5>
+
+        <div className="score pt-2 text-2xl">
+          <p>
+            <span className="text-red-700 text-2xl font-bold">Score :</span> {score}
+          </p>
+          <p>
+            <span className="text-red-700 text-2xl font-bold">Best Score :</span> {bestScore}
+          </p>
+        </div>
+
+      </div>
+
+      <Gamespace score = {score} setScore = {setScore} bestScore={bestScore} setBestScore = {setBestScore} level = {level}></Gamespace>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gamespace.jsx
@@ -0,0 +1,180 @@
+import { useState, useEffect, use } from "react";
+import "../App.css";
+
+export default function Gamespace({
+  score,
+  setScore,
+  bestScore,
+  setBestScore,
+  level,
+}) {
+  let [cards, setCards] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
+  let [pokemonData, setPokemonData] = useState([]); // Set on initial Render
+  const test = {
+    name: "kleavor",
+    src: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/900.png",
+  };
+
+  async function fetchPokemons() {
+    let limit = 40;
+    let offset = Math.floor(Math.random() * 1000);
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+
+    try {
+      const response = await fetch(url);
+
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+      throw error;
+    }
+  }
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const handleClick = (e) => {
+    const clickedCardName = e.currentTarget.querySelector("div").textContent;
+    shuffleArray(cards);
+    setCards(cards);
+    if (selectedCards.includes(clickedCardName)) {
+      if (score > bestScore) {
+        setBestScore(score);
+      }
+
+      setScore(0);
+      setSelectedCards([]);
+      console.log("Card already selected! Score reset.");
+      return;
+    }
+    setScore(score + 1);
+    // New card selected, increment score and add to selected cards
+    selectedCards.push(clickedCardName);
+    setSelectedCards(selectedCards);
+    console.log("Clicked on: " + clickedCardName);
+    console.log("Selected Cards: ", selectedCards);
+  };
+
+  async function fetcheachPokemon(pokemonArray) {
+    let finalPokemonArray = [];
+    try {
+      for (const pokemon of pokemonArray) {
+        let pokemonData = {};
+        const pokemonDetails = await fetch(pokemon.url);
+        pokemonData["name"] = pokemon.name;
+        if (pokemonDetails.ok) {
+          const pokemondetailsdata = await pokemonDetails.json();
+
+          // Prioritize higher quality images
+          let src =
+            pokemondetailsdata["sprites"]["other"]["official-artwork"][
+              "front_default"
+            ]; //higher quality
+          if (!src) {
+            src =
+              pokemondetailsdata["sprites"]["other"]["dream_world"][
+                "front_default"
+              ];
+          }
+          if (!src) {
+            src = pokemondetailsdata["sprites"]["front_default"];
+          }
+
+          // Skip Pokemon without images
+          if (src) {
+            pokemonData["src"] = src;
+            finalPokemonArray.push(pokemonData);
+          }
+        }
+      }
+      return finalPokemonArray;
+    } catch (error) {
+      console.error("Error fetching individual pokemon details:", error);
+      throw error;
+    }
+  }
+
+  function getRandomCards(pokemonData, level) {
+    const randomNumbers = [];
+    while (randomNumbers.length < level) {
+      const randomNumber = Math.floor(Math.random() * pokemonData.length);
+      if (!randomNumbers.includes(randomNumber)) {
+        randomNumbers.push(randomNumber);
+      }
+    }
+
+    let cardsData = [];
+    randomNumbers.forEach((num) => {
+      cardsData.push(pokemonData[num]);
+    });
+
+    return cardsData;
+  }
+
+  useEffect(() => {
+    fetchPokemons().then((pokemons) => {
+      console.log(pokemons);
+      const pokemonArray = pokemons.results;
+      console.log(pokemonArray);
+
+      fetcheachPokemon(pokemonArray).then((detailedPokemons) => {
+        console.log("Detailed Pokemons:", detailedPokemons);
+        const pokemons = detailedPokemons;
+        setPokemonData(pokemons); // 40 pokemons set
+
+        // let cardsData = getRandomCards(pokemons, level);
+        // setCards(cardsData);
+        // console.log("Cards Data:", cardsData);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (pokemonData.length > 0) {
+      let cardsData = getRandomCards(pokemonData, level);
+      setCards(cardsData);
+      console.log("Cards Data:", cardsData);
+    }
+    setScore(0);
+    setSelectedCards([]);
+  }, [level, pokemonData]);
+
+  return (
+    <div className="gamespace">
+      {cards.map((card) => (
+        <div
+          key={card.name}
+          className="card bg-white p-1.5"
+          onClick={handleClick}
+        >
+          <img
+            src={card.src}
+            alt={card.name}
+            className=""
+            loading="lazy"
+            onError={(e) => {
+              e.target.style.display = "none";
+            }}
+          />
+          <div className="text-white bg-red-700 w-full p-1 rounded-lg text-center capitalize">
+            {card.name}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
